840c9ff19867d3dc8721240a3e0a061940ed7d33
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-09-17 17:55:19 +0800

    基础类型

--- basics/基础/02基础数据类型.py
+++ basics/基础/02基础数据类型.py
@@ -51,46 +51,118 @@ info = '''
 # xxx # 代表访问变量名字
 'xxx'  # 代表的是值
 
+# 其他使用：
+# 字符串的嵌套，注意：外层用单引号，内存应该用双引号，反之亦然
+# print("my name is 'egon'")
+# print('my name is \'egon\'')
 
 
+# 字符串之间可以相加，但仅限于str与str之间进行，
+# 代表字符串的拼接，了解即可，不推荐使用，因为str之间的
+# 相加效率极低
+# print('my name is '+'egon')
+# print('='*20)
+# print('hello world')
+# print('='*20)
 
 
+# 3、列表：索引对应值，索引从0开始，0代表第一个
+# 作用：按位置记录多个值（同一个人的多个爱好、同一个班级的所有学校姓名、同一个人12个月的薪资），并且可以按照索引取指定位置的值
+# 定义：在[]内用逗号分隔开多个任意类型的值,一个值称之为一个元素
+#  0   1    2        3          4
+l = [10, 3.1, 'aaa', ['bbb', 'ccc'], 'ddd']
+# print(l)
+# print(l[1])
+# print(l[2])
+# print(l[3][1])
+
+# print(l[4])
+# print(l[-1]) # 获取最后一个
+
+# hobbies='read music play'
+# print(hobbies)
+# hobbies = ['read', 'music', 'play']
+# print(hobbies[1])
+
+# 其他的用途：
+# students_info=[
+#     ['tony',18,['jack',]],
+#     ['jason',18,['play','sleep']]
+# ]
+# 取出第一个学生的第一个爱好
+# print(students_info[0][2][0])
 
 
+# 4、
+# 索引反映的是顺序、位置，对值没有描述性的功能
+#      0      1   2     3
+info = ['egon', 18, 'male', 19]
+# print(type(info))
+# print(info[0])
+# print(info[1])
+# print(info[2])
 
 
+# 字典类型：key对应值，其中key通常为字符串类型，所以key对值可以有描述性的功能
+# 作用：用来存多个值，每个值都有唯一一个key与其对应，key对值有描述性功能
+# 定义：在{}内用逗号分开各多个key：value
+# d={'a':1,'b':2}
+# print(type(d))
+# print(d['a'])
 
 
+# info={
+#     "name":'egon',
+#     "age":18,
+#     "gender":'male',
+#     "salary":19
+# }
+# print(info["salary"])
+
+# 其他用途：
+# students_info=[
+#     第1个信息,
+#     第2个信息,
+#     第3个信息,
+# ]
+
+students_info = [
+    {"name": 'egon1', 'age1': 19, 'gender': 'male'},
+    {"name": 'egon2', 'age1': 19, 'gender': 'male'},
+    {"name": 'egon3', 'age1': 19, 'gender': 'male'},
+]
+
+print(students_info[1]['gender'])
+
+# 5 布尔bool
+# 6.1 作用
+# 用来记录真假这两种状态
+#
+# 6.2 定义
+# is_ok = True
+# is_ok = False
+# print(type(is_ok))
+
+# x=1
+# y=0
+
+# students=[
+#     {'name':'egon','gender':True},
+#     {'name':'alex','gender':False},
+# ]
+
+students = [
+    {'name': 'egon', 'gender': 1},
+    {'name': 'alex', 'gender': 0},
+]
+
+# 6.3 其他使用
+# 通常用来当作判断的条件，我们将在if判断中用到它
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 总结：如何选择合适的类型来记录状态
+# 1、选取的类型是否可以明确标识事物的状态
+# 2、存不是目的，存的目的是为了日后取出来用，并且方便的用
+# 3、把自己想象成一台计算机，如果我是计算机，
+#    我会如何以何种形式把事物的状态记到脑子里
+#    然后再去python中找相应的数据类型来让计算机像自己一样去记下事物的状态
