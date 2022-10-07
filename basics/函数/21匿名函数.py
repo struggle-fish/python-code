9d136b284b522b51fb9aa8bc37739a2ebf0cdcc1
jingchaoyang <jingchaoyang@autohome.com.cn>
2022-10-07 16:20:28 +0800

    匿名函数

---
+++ basics/函数/21匿名函数.py
@@ -0,0 +1,21 @@
+# 1、def用于定义有名函数
+# func=函数的内存地址
+# def func(x,y):
+#     return x+y
+
+# print(func)
+# 2、lamdab用于定义匿名函数
+# print(lambda x,y:x+y)
+
+
+# 3、调用匿名函数
+# 方式一：
+# res=(lambda x,y:x+y)(1,2)
+# print(res)
+
+# 方式二：
+# func=lambda x,y:x+y
+# res=func(1,2)
+# print(res)
+
+# 4、匿名用于临时调用一次的场景：更多的是将匿名与其他函数配合使用
